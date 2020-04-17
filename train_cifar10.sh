2bce6bb75a0e8f8556c464d2e33c201502a5c650
Seungwook Han <sh3264@coumbia.edu>
2020-04-17 14:49:16 -0400

    Cifar example and train script

---
+++ train_cifar10.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 cifar10_example.py --data=/disk_c/han/data/imagenet/10c/ --outd=./cifar_outd/ --outf=./cifar_outf/ --outl=./cifar_outl/ --outm=./cifar_outm \
+--WS --PN --saveimages=1 --savemodel=20
