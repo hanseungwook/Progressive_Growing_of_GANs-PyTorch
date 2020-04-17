cf3e8387d2864c92ed06812a8d2f664df08c5661
Seungwook Han <sh3264@coumbia.edu>
2020-04-17 12:33:23 -0400

    Removed duplicate arg

--- train_imagenet32.sh
+++ train_imagenet32.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 imagenet32_example.py --data=/hdd_e/han/data/imagenet/10c/ --outd=./imagenet32_outd/ --outf --outf=./imagenet32_outf/ --outl=./imagenet32_outl/ --outm=./imagenet32_outm \
+python3 imagenet32_example.py --data=/hdd_e/han/data/imagenet/10c/ --outd=./imagenet32_outd/ --outf=./imagenet32_outf/ --outl=./imagenet32_outl/ --outm=./imagenet32_outm \
 --BN --WS --PN --saveimages=1 --savemodel=20 --savemaxsize
